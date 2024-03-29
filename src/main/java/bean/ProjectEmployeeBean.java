0a18b00614e27bdac6df81c295df4f435349db34
ItsMasl01 <meryamassemlali@gmail.com>
2024-03-29 15:50:23 +0000

    first commit

--- src/main/java/bean/ProjectEmployeeBean.java
+++ src/main/java/bean/ProjectEmployeeBean.java
@@ -1,5 +1,3 @@
-package bean;
-
 import jakarta.faces.bean.ManagedBean;
 import jakarta.faces.bean.ViewScoped;
 import java.io.Serializable;
@@ -13,32 +11,19 @@ public class ProjectEmployeeBean implements Serializable {
     private String selectedProject;
     private String implication;
 
-    // Méthode pour récupérer la liste des noms des employés
     public List<String> getEmployeeNames() {
-        // Ici, tu devras implémenter la logique pour récupérer les noms des employés depuis ta base de données
         List<String> employeeNames = new ArrayList<>();
-        // Ajoute tes employés à la liste employeeNames
         return employeeNames;
     }
 
-    // Méthode pour récupérer la liste des noms des projets
     public List<String> getProjectNames() {
-        // Ici, tu devras implémenter la logique pour récupérer les noms des projets depuis ta base de données
         List<String> projectNames = new ArrayList<>();
-        // Ajoute tes projets à la liste projectNames
         return projectNames;
     }
 
-    // Méthode pour affecter un projet à un employé avec l'implication spécifiée
     public void assignProject() {
-        // Ici, tu devras implémenter la logique pour affecter le projet à l'employé avec l'implication spécifiée
-        System.out.println("Employee: " + selectedEmployee);
-        System.out.println("Project: " + selectedProject);
-        System.out.println("Implication: " + implication);
-        // Ajoute ta logique d'affectation du projet à l'employé ici
     }
 
-    // Getters et Setters pour les propriétés
     public String getSelectedEmployee() {
         return selectedEmployee;
     }
